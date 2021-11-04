f351b5c664be256f03b6109082b37dc8cd83f48d
Kent D <kent.daleng@nav.no>
2021-11-04 09:48:15 +0100

    add tab completion for gateways

--- cmd/root/device/jita.go
+++ cmd/root/device/jita.go
@@ -1,10 +1,12 @@
 package device
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
 	"github.com/nais/device/pkg/device-agent/open"
+	"github.com/nais/device/pkg/pb"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,12 @@ var jitaCmd = &cobra.Command{
 	Use:     "jita",
 	Short:   "Connects to a JITA gateway",
 	Example: `nais device jita [gateway]`,
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+		return getGatewayList(cmd.Context(), toComplete), cobra.ShellCompDirectiveNoFileComp
+	},
 	RunE: func(command *cobra.Command, args []string) error {
 		if len(args) != 1 {
 			return fmt.Errorf("missing required arguments: gateway")
@@ -32,4 +40,31 @@ var jitaCmd = &cobra.Command{
 func accessPrivilegedGateway(gatewayName string) error {
 	return open.Open(fmt.Sprintf("https://naisdevice-jita.nais.io/?gateway=%s", gatewayName))
 
+}
+
+func getGatewayList(ctx context.Context, toComplete string) []string {
+	connection, err := agentConnection()
+	if err != nil {
+		return nil
+	}
+
+	client := pb.NewDeviceAgentClient(connection)
+	defer connection.Close()
+
+	stream, err := client.Status(ctx, &pb.AgentStatusRequest{
+		KeepConnectionOnComplete: true,
+	})
+
+	status, err := stream.Recv()
+	if err != nil {
+		return nil
+	}
+
+	ret := make([]string, 0)
+	for _, gw := range status.Gateways {
+		if !gw.Healthy && gw.RequiresPrivilegedAccess && strings.Contains(gw.Name, strings.ToLower(toComplete)) {
+			ret = append(ret, gw.Name)
+		}
+	}
+	return ret
 }
