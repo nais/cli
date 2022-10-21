2bbc21f1c3f0142862c5c1d0c682fdea21eeb34d
Trong Huu Nguyen <tronghn@users.noreply.github.com>
2022-10-21 12:13:16 +0200

    aiven: disallow overwrite of aivenapplication if owned by another resource (#124)

--- pkg/aiven/aiven_test.go
+++ pkg/aiven/aiven_test.go
@@ -1,15 +1,17 @@
 package aiven
 
 import (
-	"github.com/nais/cli/pkg/aiven/services"
-	"github.com/nais/cli/pkg/common"
-	"github.com/nais/cli/pkg/test"
+	"testing"
+	"time"
+
 	aiven_nais_io_v1 "github.com/nais/liberator/pkg/apis/aiven.nais.io/v1"
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
-	"time"
+
+	"github.com/nais/cli/pkg/aiven/services"
+	"github.com/nais/cli/pkg/common"
+	"github.com/nais/cli/pkg/test"
 )
 
 const (
@@ -75,6 +77,39 @@ func TestAivenGenerateApplicationUpdated(t *testing.T) {
 	assert.Equal(t, services.NavDev.String(), currentAivenApp.Spec.Kafka.Pool, "Pool has the same value")
 
 	assert.True(t, currentAivenApp.Spec.ExpiresAt.After(time.Now()), "Parsed date is still valid")
+}
+
+func TestAivenGenerateApplicationUpdated_HasOwnerReference(t *testing.T) {
+	aivenApp := aiven_nais_io_v1.AivenApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      username,
+			Namespace: team,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion: "nais.io/v1alpha1",
+					Kind:       "Application",
+					Name:       username,
+					UID:        "12345",
+				},
+			},
+		},
+	}
+
+	namespace := v1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Namespace",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: team,
+		},
+	}
+
+	fakeClient := test.BuildWithScheme(&namespace, &aivenApp).Build()
+	kafka := &services.Kafka{}
+	aiven := Setup(fakeClient, kafka, username, team, secretName, "", services.NavDev, services.Read, expiry)
+	_, err := aiven.GenerateApplication()
+	assert.EqualError(t, err, "create/update: username 'user' is owned by another resource; overwrite is not allowed")
 }
 
 func TestAiven_SetSecretName(t *testing.T) {
