daf095a6e451809ba7cf4ebbf06729c3b953c9a2
Morten Lied Johansen <morten.lied.johansen@nav.no>
2024-09-17 14:54:33 +0200

    Add success message for promote command
    Co-authored-by: Trong Nguyen <trong.huu.nguyen@nav.no>

--- pkg/postgres/migrate/promote.go
+++ pkg/postgres/migrate/promote.go
@@ -5,6 +5,25 @@ import (
 	"fmt"
 )
 
+const PromoteSuccessMessage = `
+Promotion has been started successfully.
+
+To monitor the migration, run the following command:
+	kubectl logs -f -l %s -n %s
+
+The promote will take some time to complete, you can check completion status with the following command:
+	kubectl get job %s -n %s
+
+When promotion is complete, your application should be up and running with the new database instance.
+At this point it is important to verify that your application works as expected, and that all data is present.
+
+Once you are satisfied that everything works as expected, you must perform the final cleanup step:
+	nais postgres migrate cleanup %s %s %s
+
+If things are not working as expected, and you need to rollback to the previous database instance, you can run:
+	nais postgres migrate rollback %s %s %s
+`
+
 func (m *Migrator) Promote(ctx context.Context) error {
 	fmt.Println("Resolving config")
 	cfgMap, err := m.cfg.PopulateFromConfigMap(ctx, m.client)
@@ -23,5 +42,7 @@ func (m *Migrator) Promote(ctx context.Context) error {
 		return err
 	}
 
+	label := fmt.Sprintf("migrator.nais.io/migration-name=%s", m.cfg.MigrationName())
+	fmt.Printf(PromoteSuccessMessage, label, m.cfg.Namespace, job.Name, m.cfg.Namespace, m.cfg.AppName, m.cfg.Namespace, m.cfg.Target.InstanceName, m.cfg.AppName, m.cfg.Namespace, m.cfg.Target.InstanceName)
 	return nil
 }
