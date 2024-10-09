85ca4bfa3c5764b996a276f659f4b28eaf41226b
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-10-09 14:01:01 +0200

    cmd/postgrescmd: remove feature flag for migrate subcommand
    Co-authored-by: Morten Lied Johansen <morten.lied.johansen@nav.no>

--- cmd/postgrescmd/postgres.go
+++ cmd/postgrescmd/postgres.go
@@ -4,23 +4,18 @@ import (
 	"github.com/nais/cli/cmd/postgrescmd/migratecmd"
 	"github.com/nais/cli/pkg/gcp"
 	"github.com/urfave/cli/v2"
-	"os"
 )
 
 func Command() *cli.Command {
 	commands := []*cli.Command{
 		grantCommand(),
+		migratecmd.Command(),
 		passwordCommand(),
 		prepareCommand(),
 		proxyCommand(),
 		psqlCommand(),
 		revokeCommand(),
 		usersCommand(),
-	}
-
-	// Poor mans feature flag for enabling migrate command
-	if os.Getenv("NAIS_ENABLE_MIGRATE") == "true" {
-		commands = append(commands, migratecmd.Command())
 	}
 
 	return &cli.Command{
