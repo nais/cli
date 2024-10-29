cbd9307782546a5076f3bf0677507969079356e2
sindrerh2 <sindre.rodseth.hansen@nav.no>
2024-10-29 15:35:39 +0100

    feat: adding flag metrics
    Co-authored-by: ybelmekk <youssef.bel.mekki@nav.no>
    Co-authored-by: Carl Hedgren<carl.hedgren@nav.no>

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"context"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"log"
 	"os"
+	"time"
 
 	"github.com/nais/cli/cmd/aivencmd"
 	"github.com/nais/cli/cmd/appstartercmd"
@@ -12,6 +19,8 @@ import (
 	"github.com/nais/cli/cmd/rootcmd"
 	"github.com/nais/cli/cmd/validatecmd"
 	"github.com/urfave/cli/v2"
+
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 var (
@@ -44,8 +53,77 @@ func Run() {
 		Commands:             commands(),
 	}
 
+	var validSubcommands []string
+	for _, command := range app.Commands {
+		validSubcommands = append(validSubcommands, command.Name)
+		for _, subcommand := range command.Subcommands {
+			validSubcommands = append(validSubcommands, subcommand.Name)
+		}
+	}
+
+	meterProv := New()
+	defer meterProv.Shutdown(context.Background())
+	commandHistogram, _ := meterProv.Meter("nais-cli").Int64Histogram("flag_usage", metric.WithDescription("Usage frequency of command flags"))
+	ctx := context.Background()
+	recordCommandUsage(ctx, commandHistogram, intersection(os.Args, validSubcommands))
+	meterProv.ForceFlush(context.Background())
+
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func NewResource() (*resource.Resource, error) {
+	return resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName("nais_cli"),
+			semconv.ServiceVersion(version+":"+commit),
+		))
+}
+
+func NewMeterProvider(res *resource.Resource) *sdkmetric.MeterProvider {
+	dnt := os.Getenv("DO_NOT_TRACK")
+	var url string
+	if dnt == "1" {
+		url = "http://localhost"
+	} else {
+		url = "https://collector-internet.nav.cloud.nais.io"
+	}
+	metricExporter, _ := otlpmetrichttp.New(
+		context.Background(),
+		otlpmetrichttp.WithEndpointURL(url),
+	)
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(res),
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter,
+			sdkmetric.WithInterval(1*time.Second))),
+	)
+	return meterProvider
+}
+
+func New() *sdkmetric.MeterProvider {
+	res, _ := NewResource()
+	meterProvider := NewMeterProvider(res)
+	return meterProvider
+}
+
+func recordCommandUsage(ctx context.Context, histogram metric.Int64Histogram, flags []string) {
+	for _, flag := range flags {
+		histogram.Record(ctx, 1, metric.WithAttributes(attribute.String("flag", flag)))
+	}
+}
+
+func intersection(list1, list2 []string) []string {
+	elements := make(map[string]bool)
+	for _, item := range list1 {
+		elements[item] = true
+	}
+	var result []string
+	for _, item := range list2 {
+		if elements[item] {
+			result = append(result, item)
+		}
+	}
+	return result
 }
