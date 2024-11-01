9b7ea51bfa0aa518fe781f494cfc3e5c3b03b95c
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2024-11-01 14:19:59 +0100

    fix(metrics): use same format for version in metrics
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    Co-authored-by: Carl Hedgren <carl.hedgren@nav.no>

--- pkg/metrics/otel.go
+++ pkg/metrics/otel.go
@@ -29,7 +29,7 @@ func newResource() (*resource.Resource, error) {
 	return resource.Merge(resource.Default(),
 		resource.NewWithAttributes(semconv.SchemaURL,
 			semconv.ServiceName("nais_cli"),
-			semconv.ServiceVersion(version+":"+commit),
+			semconv.ServiceVersion(version+"-"+commit),
 		))
 }
 
