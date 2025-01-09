98303251c6898dc5f5662a9eb6b086e981215192
carl hedgren <carl.hedgren@nav.no>
2025-01-09 14:29:06 +0100

    fall-throuhg for other binaries

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/nais/cli/cmd/debugcmd"
 
@@ -48,8 +49,39 @@ func Run() {
 
 	m.CollectCommandHistogram(app.Commands)
 
+	if len(os.Args) > 1 {
+		if !isCommand(os.Args[1], app.Commands) {
+			binaryName := "nais-" + os.Args[1]
+			if err := runOtherBin(binaryName, os.Args[2:]); err != nil {
+			}
+		}
+	}
+
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func isCommand(command string, commands []*cli.Command) bool {
+	for _, cmd := range commands {
+		if cmd.Name == command {
+			return true
+		}
+	}
+	return false
+}
+
+func runOtherBin(binary string, args []string) error {
+	binaryPath, err := exec.LookPath(binary)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(binaryPath, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	return cmd.Run()
 }
