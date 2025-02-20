9d3551dc97a866c8f99d0dfb2480ff6f40f52724
carl hedgren <carl.hedgren@nav.no>
2025-02-20 22:47:24 +0100

    Bump go version
    Jesus christ what is up with the go people?
    Lets pin patch versions of our toolchain.
    I can't even

--- flake.nix
+++ flake.nix
@@ -3,63 +3,61 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    version = builtins.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101");
-    # goOverlay = final: prev: {
-    #   go = prev.go.overrideAttrs (old: {
-    #     version = "1.23.2";
-    #     src = prev.fetchurl {
-    #       url = "https://go.dev/dl/go1.23.2.src.tar.gz";
-    #       hash = "sha256-NpMBYqk99BfZC9IsbhTa/0cFuqwrAkGO3aZxzfqc0H8=";
-    #     };
-    #   });
-    # };
-    withSystem = nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-    withPkgs = callback:
-      withSystem (
-        system:
-          callback (
-            import nixpkgs {
-              inherit system;
-              # overlays = [goOverlay];
-            }
-          )
-      );
-  in {
-    packages = withPkgs (pkgs: rec {
-      nais = pkgs.buildGoModule {
-        pname = "nais-cli";
-        inherit version;
-        src = ./.;
-        vendorHash = "sha256-WE5WSOOAUBEfxF/NAaVjTFBZeMRA3APmOJGbBueKObo=";
-        postInstall = ''
-          mv $out/bin/cli $out/bin/nais
-        '';
-      };
-      default = nais;
-    });
+  outputs = { self, nixpkgs, }:
+    let
+      version = builtins.substring 0 8
+        (self.lastModifiedDate or self.lastModified or "19700101");
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      withPkgs = callback:
+        withSystem (system:
+          callback (import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                go = (prev.go.overrideAttrs {
+                  version = "1.23.6";
+                  src = prev.fetchurl {
+                    url = "https://go.dev/dl/go1.23.6.src.tar.gz";
+                    hash =
+                      "sha256-A5xbBOZSedrO7opvcecL0Fz1uAF4K293xuGeLtBREiI=";
+                  };
+                });
+              })
+            ];
+          }));
+    in {
+      packages = withPkgs (pkgs: rec {
+        nais = pkgs.buildGoModule.override { go = pkgs.go; } {
+          pname = "nais-cli";
+          inherit version;
+          src = ./.;
+          vendorHash = "sha256-kr9VARylPRphfrBEf8KgY1mCV+a1lwQpT/lpur1T3tQ=";
 
-    devShells = withPkgs (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go
-          gopls
-          gotools
-          go-tools
-          nodejs_20
-          nodePackages.prettier
-        ];
-      };
-    });
+          postInstall = ''
+            mv $out/bin/cli $out/bin/nais
+          '';
+        };
+        default = nais;
+      });
 
-    formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
-  };
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+            nodejs_20
+            nodePackages.prettier
+          ];
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
 }
