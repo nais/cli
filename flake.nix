48c7360484e4e0f745f54342387e76e57a6c19c5
carl hedgren <carl.hedgren@nav.no>
2025-04-29 13:39:18 +0200

    add mise

--- flake.nix
+++ flake.nix
@@ -7,7 +7,12 @@
   outputs =
     { self, ... }@inputs:
     inputs.flake-utils.lib.eachSystem
-      [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]
+      [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ]
       (
         system:
         let
@@ -57,8 +62,7 @@
               gopls
               gotools
               go-tools
-              nodejs_20
-              nodePackages.prettier
+              mise
             ];
           };
 
