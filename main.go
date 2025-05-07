8b9e26d9fd2747277501c90418d9b488578e5641
Christer Edvartsen <christer.edvartsen@nav.no>
2025-05-07 11:52:51 +0200

    refactor: hide duplicate error for now

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"github.com/nais/cli/internal/cli"
@@ -10,7 +9,8 @@ import (
 
 func main() {
 	if err := cli.Run(context.Background()); err != nil {
-		fmt.Println(err)
+		// TODO: differentiate between cobra errors and internal errors
+		// fmt.Println(err)
 		os.Exit(1)
 	}
 }
