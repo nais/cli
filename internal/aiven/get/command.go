9c5992b9985395d6080c0e8e32d41c1acd9470cc
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-05-15 09:21:28 +0200

    re-add the error metrics we had previously

--- internal/aiven/get/command.go
+++ internal/aiven/get/command.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/nais/cli/internal/aiven"
 	"github.com/nais/cli/internal/aiven/aiven_services"
+	"github.com/nais/cli/internal/metric"
 )
 
 type Arguments struct {
@@ -15,6 +16,7 @@ type Arguments struct {
 
 func Run(ctx context.Context, service aiven_services.Service, args Arguments) error {
 	if err := aiven.ExtractAndGenerateConfig(ctx, service, args.SecretName, args.Namespace); err != nil {
+		metric.CreateAndIncreaseCounter(ctx, "aiven_get_secret_and_config_error_total")
 		return fmt.Errorf("retrieve secret and generating config: %w", err)
 	}
 
--- internal/postgres/audit/command.go
+++ internal/postgres/audit/command.go
@@ -3,9 +3,14 @@ package audit
 import (
 	"context"
 
+	"github.com/nais/cli/internal/metric"
 	"github.com/nais/cli/internal/postgres"
 )
 
 func Run(ctx context.Context, applicationName string, flags postgres.Flags) error {
-	return postgres.EnableAuditLogging(ctx, applicationName, flags.Context, flags.Namespace)
+	err := postgres.EnableAuditLogging(ctx, applicationName, flags.Context, flags.Namespace)
+	if err != nil {
+		metric.CreateAndIncreaseCounter(ctx, "enable_audit_logging_error")
+	}
+	return err
 }
