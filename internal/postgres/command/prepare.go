a6bdb0399ccaaf6a5ee6c04501e78c35f0281234
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-06-05 09:36:31 +0200

    fix postgres prepare indent

--- internal/postgres/command/prepare.go
+++ internal/postgres/command/prepare.go
@@ -24,10 +24,10 @@ func prepareCommand(parentFlags *flag.Postgres) *cli.Command {
 		Name:  "prepare",
 		Short: "Prepare your SQL instance for use with personal accounts.",
 		Long: `Prepare will prepare the SQL instance by connecting using the
-		 application credentials and modify the permissions on the public schema.
-		 All IAM users in your GCP project will be able to connect to the instance.
+application credentials and modify the permissions on the public schema.
+All IAM users in your GCP project will be able to connect to the instance.
 		
-		 This operation is only required to run once for each SQL instance.`,
+This operation is only required to run once for each SQL instance.`,
 		Args: []cli.Argument{
 			{Name: "app_name", Required: true},
 		},
