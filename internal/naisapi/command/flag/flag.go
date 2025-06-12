4884a2e352f8cc235ebe7a8ba24e20b456a0179b
Christer Edvartsen <christer.edvartsen@nav.no>
2025-06-12 10:42:09 +0200

    fix: autocomplete output options

--- internal/naisapi/command/flag/flag.go
+++ internal/naisapi/command/flag/flag.go
@@ -1,6 +1,10 @@
 package flag
 
-import "github.com/nais/cli/internal/root"
+import (
+	"context"
+
+	"github.com/nais/cli/internal/root"
+)
 
 type Alpha struct {
 	*root.Flags
@@ -12,15 +16,19 @@ type Api struct {
 
 type Proxy struct {
 	*Api
-
 	ListenAddr string `name:"listen" short:"l" usage:"Address the proxy will listen on."`
+}
+
+type Output string
+
+func (o *Output) AutoComplete(context.Context, []string, string, any) ([]string, string) {
+	return []string{"table", "json"}, "Available output formats."
 }
 
 type Teams struct {
 	*Api
-
-	All    bool   `name:"all" short:"a" usage:"List all teams, not just the ones you are a member of"`
-	Output string `name:"output" short:"o" usage:"Format output (table|json)"`
+	All    bool   `name:"all" short:"a" usage:"List all teams, not just the ones you are a member of."`
+	Output Output `name:"output" short:"o" usage:"Format output (table|json)."`
 }
 
 type Schema struct {
