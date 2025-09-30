36412b5f1a36405b9a3f9d975a3455a00607e92b
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-30 11:18:43 +0200

    build: unset GOOS and GOARCH

--- mise/tasks/completions.sh
+++ mise/tasks/completions.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish powershell; do
-	go run main.go completion "$sh" >"completions/nais.$sh"
+	GOOS="" GOARCH="" go run main.go completion "$sh" >"completions/nais.$sh"
 done
