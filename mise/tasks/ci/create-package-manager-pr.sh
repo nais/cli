1f75cd88609ecebdb2d688ac420112840d86cc92
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-30 10:02:09 +0200

    build: less permissions

---
+++ mise/tasks/ci/create-package-manager-pr.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#MISE description="Create PR in a package-manager repository"
+set -euo pipefail
+
+workspace="$MISE_PROJECT_ROOT"
+
+repo="$1"
+file="$2"
+
+basename="${file##*/}" # basename
+name="${basename%.*}"  # remove extension
+
+# clone repo
+repo_dir="$(mktemp -d)"
+gh repo clone "$repo" "$repo_dir" -- --depth=1
+cd "$repo_dir" || exit 1
+
+vars="$workspace/template.vars"
+
+# generate file from template
+# shellcheck disable=SC2046
+env $(xargs -0 <"$vars") \
+	envsubst "$(sed 's/^/$/' "$vars")" \
+	<"$workspace/.github/workflows/templates/${basename}" \
+	>"${file}"
+
+version=$(grep -m 1 -oP '^VERSION=.+$' "$vars" | cut -d '=' -f 2)
+
+# create pr
+branch="${name//-/_}_${version}"
+git config user.name "NAIS team app"
+git config user.email "devnull@nais.io"
+gh auth setup-git
+git switch -c "$branch"
+git commit -am "$name $version"
+git push --set-upstream origin "$branch"
+until gh pr create --fill --base main --head "$branch"; do echo "Retrying gh pr create..."; sleep 1; done
+until gh pr merge --auto --rebase; do echo "Retrying gh pr merge..."; sleep 1; done
--- mise/tasks/ci/release-info.sh
+++ mise/tasks/ci/release-info.sh
@@ -1,42 +1,86 @@
 #!/usr/bin/env bash
-#MISE description="Ensure all code is formatted"
-#MISE depends=["fmt"]
+#MISE description="Generate release information using git-cliff"
 set -euo pipefail
+
+main() {
+	local repository token changelog version
+	repository="$1"
+	token="$2"
+
+	changelog="$(git-cliff \
+		--bump \
+		--github-repo "$repository" \
+		--github-token "$token" \
+		--unreleased \
+		--verbose \
+		--strip all)"
+
+	local body
+	if [[ -n "$changelog" ]]; then
+		body=$(
+			cat <<-EOF
+				# :pencil: Changelog preview
+				Below is a preview of the Changelog that will be added to the next release. Only commit messages that follow the [Conventional Commits specification](https://www.conventionalcommits.org/) will be included in the Changelog.
+
+				$changelog
+			EOF
+		)
+	else
+		body=$(
+			cat <<-EOF
+				body="# :disappointed: No release for you
+				There are no commits in your branch that follow the [Conventional Commits specification](https://www.conventionalcommits.org/), so no release will be created.
+
+				If you want to create a release from this pull request, please reword your commit messages to replace this message with a preview of a beautiful Changelog."
+			EOF
+		)
+	fi
+
+	if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+		gh pr comment "${GITHUB_REF_NAME%%/merge}" \
+			--edit-last --create-if-none \
+			--repo "$repository" \
+			--body "$body"
+	fi
+
+	version="$(grep -m 1 -oP '(?<=^##\s)v\d+\.\d+\.\d+(?=\s-\s)' <<<"$changelog")"
+	if [[ -z "$version" ]]; then
+		echo "Unable to read version from changelog, abort"
+		return
+	fi
+
+	output "changelog" "$changelog"
+	output "version" "$version"
+}
+
+output() {
+	local key value
+	key="$1"
+	value="$2"
+
+	if [[ -z "$GITHUB_OUTPUT" ]]; then
+		echo "output: $key => $value"
+		return
+	fi
+
+	if [[ -z "$key" ]]; then
+		echo "output: missing key " >&2
+	elif [[ -z "$value" ]]; then
+		echo "output: missing value for key '$key'" >&2
+	else
+		{
+			echo "$key<<EOF"
+			echo "$value"
+			echo "EOF"
+		} >>"$GITHUB_OUTPUT"
+	fi
+}
 
 repository="${1:-$GITHUB_REPOSITORY}"
 token="${2:-$GITHUB_TOKEN}"
-
-version="$(git-cliff --bumped-version)"
-echo "version=$version" >>"$GITHUB_OUTPUT"
-echo "Bumped version: $version"
-changelog="$(git-cliff \
-  --tag "$version" \
-  --github-repo "$repository" \
-  --github-token "$token" \
-  --unreleased \
-  --strip all \
-  -v)"
-echo "changelog<<EOF" >>"$GITHUB_OUTPUT"
-echo "$changelog" >>"$GITHUB_OUTPUT"
-echo "EOF" >>"$GITHUB_OUTPUT"
-
-if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
-  echo -n "PR comment with release info: "
-  if [[ -n "$changelog" ]]; then
-    pr_comment="# :pencil: Changelog preview
-Below is a preview of the Changelog that will be added to the next release. \
-Only commit messages that follow the [Conventional Commits specification](https://www.conventionalcommits.org/) will be included in the Changelog.
-
-$changelog"
-  else
-    pr_comment="# :disappointed: No release for you
-There are no commits in your branch that follow the [Conventional Commits specification](https://www.conventionalcommits.org/), so no release will be created.
-
-If you want to create a release from this pull request, please reword your commit messages to replace this message with a preview of a beautiful Changelog."
-  fi
-
-  echo -e "$pr_comment" | gh pr comment "${GITHUB_REF_NAME%%/merge}" \
-    --edit-last --create-if-none \
-    --repo "$repository" \
-    --body-file=-
+if [[ -z "$repository" || -z "$token" ]]; then
+	echo "Usage: $0 <repository> <token>"
+	exit 1
 fi
+
+main "$repository" "$token"
