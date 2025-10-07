3cd78150a523574c0ee7c6d723bdd314889525b9
Christer Edvartsen <christer.edvartsen@nav.no>
2025-10-07 12:31:08 +0200

    feat: generate links in tables for some commands
    This is currently using an unreleased version of pterm, so should not be
    merged until a new release has been generated.

--- internal/naisapi/command/teams.go
+++ internal/naisapi/command/teams.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/nais/cli/internal/naisapi"
 	"github.com/nais/cli/internal/naisapi/command/flag"
@@ -20,13 +21,17 @@ func teamsCommand(parentFlags *flag.Api) *naistrix.Command {
 		Title: "Get a list of your teams.",
 		Flags: flags,
 		RunFunc: func(ctx context.Context, out naistrix.Output, _ []string) error {
-			// TODO: Once https://github.com/pterm/pterm/issues/697 is resolved, we can use a link to Console instead of just the slug.
-			type team struct {
-				Slug        string `json:"slug"`
+			user, err := naisapi.GetAuthenticatedUser(ctx)
+			if err != nil {
+				return err
+			}
+
+			type entry struct {
+				Team        team   `json:"team"`
 				Description string `json:"description"`
 			}
 
-			var teams []team
+			var entries []entry
 
 			if flags.All {
 				ret, err := naisapi.GetAllTeams(ctx)
@@ -35,8 +40,11 @@ func teamsCommand(parentFlags *flag.Api) *naistrix.Command {
 				}
 
 				for _, t := range ret.Teams.Nodes {
-					teams = append(teams, team{
-						Slug:        t.Slug,
+					entries = append(entries, entry{
+						Team: team{
+							Slug: t.Slug,
+							Url:  fmt.Sprintf("https://%s/team/%s", user.ConsoleHost(), t.Slug),
+						},
 						Description: t.Purpose,
 					})
 				}
@@ -47,23 +55,26 @@ func teamsCommand(parentFlags *flag.Api) *naistrix.Command {
 				}
 
 				for _, t := range userTeams {
-					teams = append(teams, team{
-						Slug:        t.Team.Slug,
+					entries = append(entries, entry{
+						Team: team{
+							Slug: t.Team.Slug,
+							Url:  fmt.Sprintf("https://%s/team/%s", user.ConsoleHost(), t.Team.Slug),
+						},
 						Description: t.Team.Purpose,
 					})
 				}
 			}
 
 			if flags.Output == "json" {
-				return out.JSON(output.JSONWithPrettyOutput()).Render(teams)
+				return out.JSON(output.JSONWithPrettyOutput()).Render(entries)
 			}
 
-			if len(teams) == 0 {
+			if len(entries) == 0 {
 				out.Println("No teams found.")
 				return nil
 			}
 
-			return out.Table().Render(teams)
+			return out.Table().Render(entries)
 		},
 	}
 }
