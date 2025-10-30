0214e57f65897ddd2731562a099487910ece1b80
Johnny Fredheim Horvi <johnny@horvi.no>
2025-10-30 13:21:41 +0100

    Fix tests

--- internal/issues/command/list_test.go
+++ internal/issues/command/list_test.go
@@ -1,7 +1,6 @@
 package command
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/nais/cli/internal/issues/command/flag"
@@ -10,13 +9,13 @@ import (
 func TestParseFilters(t *testing.T) {
 	type want struct {
 		filters *flag.Filters
-		err     error
+		err     string
 	}
 
 	tests := []struct {
 		name  string
 		input string
-		want
+		want  want
 	}{
 		{
 			name:  "single filter",
@@ -32,7 +31,7 @@ func TestParseFilters(t *testing.T) {
 			input: "environment=x,severity=CRITICAL",
 			want: want{
 				filters: &flag.Filters{
-					Environment: "y",
+					Environment: "x",
 					Severity:    "CRITICAL",
 				},
 			},
@@ -41,7 +40,14 @@ func TestParseFilters(t *testing.T) {
 			name:  "unknown filter",
 			input: "unknown=something",
 			want: want{
-				err: fmt.Errorf("unknown filter key: unknown"),
+				err: "unknown filter key: unknown",
+			},
+		},
+		{
+			name:  "malformed filter",
+			input: "environment=x,severity",
+			want: want{
+				err: "incorrect filter: severity",
 			},
 		},
 	}
@@ -50,11 +56,20 @@ func TestParseFilters(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 			got, err := parseFilter(test.input)
-			if err != test.err {
-				t.Errorf("parseFilter(%q) = %+v, want %+v", test.input, err, test.err)
+
+			if test.want.err != "" {
+				if err == nil {
+					t.Errorf("parseFilter(%q) = %+v, want error %+v", test.input, err, test.want.err)
+				}
+
+				if err.Error() != test.want.err {
+					t.Errorf("parseFilter(%q) = %+v, want %+v", test.input, err, test.want.err)
+				}
+				return
 			}
+
 			if *got != *test.want.filters {
-				t.Errorf("parseFilter(%q) = %+v, want %+v", test.input, got, test.want)
+				t.Errorf("parseFilter(%q) = %+v, want %+v", test.input, got, test.want.filters)
 			}
 		})
 	}
