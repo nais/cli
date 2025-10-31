ac9c3f7263c2ca0973f1449da8978756cbd1187c
Johnny Fredheim Horvi <johnny@horvi.no>
2025-10-31 14:07:40 +0100

    wip

--- internal/issues/command/flag/flag.go
+++ internal/issues/command/flag/flag.go
@@ -1,7 +1,10 @@
 package flag
 
 import (
+	"context"
+
 	"github.com/nais/cli/internal/alpha/command/flag"
+	"github.com/nais/naistrix"
 )
 
 type Issues struct {
@@ -10,5 +13,11 @@ type Issues struct {
 
 type List struct {
 	*Issues
-	Filter string `name: "filter", usage:"filter"`
+	Filter   string   `name:"filter" usage:"Filter output (environment,severity,resourceType,resourceName,issueType)=value,..."`
+	Severity Severity `name:"severity" usage:"Filter issues by severity (CRITICAL, WARNING, TODO)"`
+}
+type Severity string
+
+func (s *Severity) AutoComplete(context.Context, *naistrix.Arguments, string, any) ([]string, string) {
+	return []string{"CRITICAL", "WARNING", "TODO"}, "Available severities"
 }
