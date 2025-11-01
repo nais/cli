dcb9d279e1ab6783cdca87550ff37dca65bb0ab8
Christer Edvartsen <christer.edvartsen@nav.no>
2025-11-01 11:51:30 +0100

    feat: gateway command
    Start working on a gateway command to interact with naisdevice gateways.

--- internal/naisdevice/command/flag/flag.go
+++ internal/naisdevice/command/flag/flag.go
@@ -15,8 +15,29 @@ func (o *Output) AutoComplete(context.Context, *naistrix.Arguments, string, any)
 	return []string{"yaml", "json"}, "Available output formats."
 }
 
-type Status struct {
+type Device struct {
 	*flags.GlobalFlags
+}
+
+type Status struct {
+	*Device
 	Quiet  bool   `name:"quiet" short:"q" usage:"Suppress output"`
 	Output Output `name:"output" short:"o" usage:"Format output (yaml|json)."`
+}
+
+type Gateway struct {
+	*Device
+}
+
+type List struct {
+	*Gateway
+	Output Output `name:"output" short:"o" usage:"Format output (yaml|json)."`
+}
+
+type Describe struct {
+	*Gateway
+}
+
+type Connect struct {
+	*Gateway
 }
--- internal/naisdevice/command/naisdevice.go
+++ internal/naisdevice/command/naisdevice.go
@@ -2,15 +2,18 @@ package command
 
 import (
 	"github.com/nais/cli/internal/flags"
+	"github.com/nais/cli/internal/naisdevice/command/flag"
 	"github.com/nais/naistrix"
 )
 
 func Naisdevice(parentFlags *flags.GlobalFlags) *naistrix.Command {
+	flags := &flag.Device{GlobalFlags: parentFlags}
 	return &naistrix.Command{
 		Name:  "device",
 		Title: "Interact with naisdevice",
 		SubCommands: []*naistrix.Command{
-			statuscmd(parentFlags),
+			statuscmd(flags),
+			gatewaycmd(flags),
 			jitacmd(),
 			doctorcmd(),
 			disconnectcmd(),
