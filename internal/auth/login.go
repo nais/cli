9c3d5b16f97a2e35e257687095defcdaa1bb5092
Morten Lied Johansen <morten.lied.johansen@nav.no>
2025-11-10 15:48:58 +0100

    feat: Introduce new auth command group and move login/logout there
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>

--- internal/auth/login.go
+++ internal/auth/login.go
@@ -1,9 +1,9 @@
-package command
+package auth
 
 import (
 	"context"
 
-	"github.com/nais/cli/internal/auth"
+	"github.com/nais/cli/internal/auth/flag"
 	"github.com/nais/cli/internal/flags"
 	"github.com/nais/cli/internal/gcloud"
 	"github.com/nais/cli/internal/naisapi"
@@ -11,13 +11,18 @@ import (
 )
 
 type loginFlags struct {
-	*flags.GlobalFlags
+	*flag.Auth
 	Nais bool `name:"nais" short:"n" usage:"Login using login.nais.io instead of gcloud."`
 }
 
-func Login(parentFlags *flags.GlobalFlags) *naistrix.Command {
-	flags := &loginFlags{GlobalFlags: parentFlags}
-	return &naistrix.Command{
+func LoginDeprecated(parentFlags *flags.GlobalFlags) *naistrix.Command {
+	flags := &flag.Auth{GlobalFlags: parentFlags}
+	return Login(flags, Deprecated)
+}
+
+func Login(parentFlags *flag.Auth, modifiers ...func(*naistrix.Command)) *naistrix.Command {
+	flags := &loginFlags{Auth: parentFlags}
+	cmd := &naistrix.Command{
 		Name:  "login",
 		Title: "Log in to the Nais platform.",
 		Examples: []naistrix.Example{
@@ -30,7 +35,6 @@ func Login(parentFlags *flags.GlobalFlags) *naistrix.Command {
 			},
 		},
 		Description: `Uses "gcloud auth login --update-adc" by default.`,
-		Group:       auth.GroupName,
 		Flags:       flags,
 		RunFunc: func(ctx context.Context, _ *naistrix.Arguments, out *naistrix.OutputWriter) error {
 			if flags.Nais {
@@ -40,4 +44,8 @@ func Login(parentFlags *flags.GlobalFlags) *naistrix.Command {
 			return gcloud.Login(ctx, out, flags.IsVerbose())
 		},
 	}
+	for _, modifier := range modifiers {
+		modifier(cmd)
+	}
+	return cmd
 }
