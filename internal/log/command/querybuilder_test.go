85153c762a407e988b2c542f3933599e36081baf
Johnny Fredheim Horvi <johnny@horvi.no>
2025-12-02 15:11:18 +0100

    fix: use correct name when adding pods

--- internal/log/command/querybuilder_test.go
+++ internal/log/command/querybuilder_test.go
@@ -22,6 +22,7 @@ func TestQueryBuilder_Build(t *testing.T) {
 			AddTeams("t1", "t2").
 			AddWorkloads("w1", "w2").
 			AddContainers("c1", "c2").
+			AddPods("p1", "p2").
 			Build()
 
 		expected := `{` +
@@ -30,7 +31,8 @@ func TestQueryBuilder_Build(t *testing.T) {
 			`,service_namespace=~"t1|t2"` +
 			`,service_name=~"w1|w2"` +
 			`}` +
-			` | k8s_container_name=~"c1|c2"`
+			` | k8s_container_name=~"c1|c2"` +
+			` | k8s_pod_name=~"p1|p2"`
 
 		if query != expected {
 			t.Errorf("Expected query to be %q, got %q", expected, query)
