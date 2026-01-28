7137418d34d6991ac1f6aead1389184ed3c13374
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2026-01-28 08:59:02 +0100

    fix(naisapi/auth): store credentials in encrypted file instead of keyring
    This changes nais-cli to store session credentials as an encrypted file
    instead of storing the entire session in the keyring.
    
    As the session includes tokens of varying length, the value may exceed
    limits imposed by credential managers on various systems, most notably
    Windows with a limit of around 2560 bytes.
    
    By generating an encryption key and only storing that in the keyring, we
    circumvent these size limitations. New keys are generated and stored in
    the keyring every time the credential file is encrypted and written to disk.

--- internal/keyring/keyring.go
+++ internal/keyring/keyring.go
@@ -1,9 +1,10 @@
 // keyring is a simple wrapper that adds timeouts to the zalando/go-keyring package
 // "Borrowed" with ❤️ from https://github.com/cli/cli/blob/17af24e147629aa1aed2546e87e9323aeabf4c8c/internal/keyring/keyring.go
 
-package auth
+package keyring
 
 import (
+	"encoding/base64"
 	"errors"
 	"time"
 
@@ -15,31 +16,17 @@ const (
 	keyringUser = "nais-user"
 )
 
-var errSecretNotFound = errors.New("secret not found in keyring")
+var ErrSecretNotFound = errors.New("secret not found in keyring")
 
-type timeoutError struct {
+type TimeoutError struct {
 	message string
 }
 
-func (e *timeoutError) Error() string {
+func (e *TimeoutError) Error() string {
 	return e.message
 }
 
-func setKeyringSecret(secret string) error {
-	ch := make(chan error, 1)
-	go func() {
-		defer close(ch)
-		ch <- keyring.Set(serviceName, keyringUser, secret)
-	}()
-	select {
-	case err := <-ch:
-		return err
-	case <-time.After(3 * time.Second):
-		return &timeoutError{"timeout while trying to set secret in keyring"}
-	}
-}
-
-func getKeyringSecret() (string, error) {
+func Get() (string, error) {
 	ch := make(chan struct {
 		val string
 		err error
@@ -55,15 +42,41 @@ func getKeyringSecret() (string, error) {
 	select {
 	case res := <-ch:
 		if errors.Is(res.err, keyring.ErrNotFound) {
-			return "", errSecretNotFound
+			return "", ErrSecretNotFound
 		}
 		return res.val, res.err
 	case <-time.After(3 * time.Second):
-		return "", &timeoutError{"timeout while trying to get secret from keyring"}
+		return "", &TimeoutError{"timeout while trying to get secret from keyring"}
 	}
 }
 
-func deleteKeyringSecret() error {
+func GetBytes() ([]byte, error) {
+	encoded, err := Get()
+	if err != nil {
+		return nil, err
+	}
+	return base64.StdEncoding.DecodeString(encoded)
+}
+
+func Set(value string) error {
+	ch := make(chan error, 1)
+	go func() {
+		defer close(ch)
+		ch <- keyring.Set(serviceName, keyringUser, value)
+	}()
+	select {
+	case err := <-ch:
+		return err
+	case <-time.After(3 * time.Second):
+		return &TimeoutError{"timeout while trying to set secret in keyring"}
+	}
+}
+
+func SetBytes(value []byte) error {
+	return Set(base64.StdEncoding.EncodeToString(value))
+}
+
+func Delete() error {
 	ch := make(chan error, 1)
 	go func() {
 		defer close(ch)
@@ -72,10 +85,10 @@ func deleteKeyringSecret() error {
 	select {
 	case err := <-ch:
 		if errors.Is(err, keyring.ErrNotFound) {
-			return errSecretNotFound
+			return ErrSecretNotFound
 		}
 		return err
 	case <-time.After(3 * time.Second):
-		return &timeoutError{"timeout while trying to delete secret from keyring"}
+		return &TimeoutError{"timeout while trying to delete keyring secrets for service"}
 	}
 }
