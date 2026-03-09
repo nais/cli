1e9a00076c9a6ddfedc499cb81db5110a1057486
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-09 14:58:24 +0100

    feat(app): make autocomplete environment-aware

--- internal/app/command/issues.go
+++ internal/app/command/issues.go
@@ -39,7 +39,10 @@ func issues(parentFlags *flag.App) *naistrix.Command {
 		},
 		AutoCompleteFunc: func(ctx context.Context, args *naistrix.Arguments, _ string) ([]string, string) {
 			if args.Len() == 0 {
-				apps, err := app.GetApplicationNames(ctx, flags.Team)
+				if len(flags.Team) == 0 {
+					return nil, "Please provide team to auto-complete application names. 'nais config set team <team>', or '--team <team>' flag."
+				}
+				apps, err := app.GetApplicationNames(ctx, flags.Team, flags.Environment)
 				if err != nil {
 					return nil, "Unable to fetch application names."
 				}
