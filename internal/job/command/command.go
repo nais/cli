1c5a09dd5acc0cdb62843c95e1c4b1d47383fc42
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-09 09:50:42 +0100

    feat(job): add job list command with GraphQL query

---
+++ internal/job/command/command.go
@@ -0,0 +1,26 @@
+package command
+
+import (
+	"context"
+
+	"github.com/nais/cli/internal/flags"
+	"github.com/nais/cli/internal/job/command/flag"
+	"github.com/nais/cli/internal/validation"
+	"github.com/nais/naistrix"
+)
+
+func Job(parentFlags *flags.GlobalFlags) *naistrix.Command {
+	flags := &flag.Job{GlobalFlags: parentFlags}
+	return &naistrix.Command{
+		Name:    "job",
+		Aliases: []string{"jobs"},
+		Title:   "Interact with jobs.",
+		ValidateFunc: func(context.Context, *naistrix.Arguments) error {
+			return validation.CheckTeam(flags.Team)
+		},
+		StickyFlags: flags,
+		SubCommands: []*naistrix.Command{
+			list(flags),
+		},
+	}
+}
