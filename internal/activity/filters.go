aa27041de7b042e53e7f4782ad75dca25a196bba
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-13 11:06:27 +0100

    refactor(activity): share enum parsing across activity commands

---
+++ internal/activity/filters.go
@@ -0,0 +1,35 @@
+package activity
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nais/cli/internal/naisapi/gql"
+)
+
+func ParseActivityTypes(in []string) ([]gql.ActivityLogActivityType, error) {
+	return parseEnum[gql.ActivityLogActivityType](in, gql.AllActivityLogActivityType, "activity type")
+}
+
+func ParseResourceTypes(in []string) ([]gql.ActivityLogEntryResourceType, error) {
+	return parseEnum[gql.ActivityLogEntryResourceType](in, gql.AllActivityLogEntryResourceType, "resource type")
+}
+
+func parseEnum[T ~string](in []string, allowedValues []T, label string) ([]T, error) {
+	ret := make([]T, 0, len(in))
+	allowed := make(map[string]T, len(allowedValues))
+	for _, v := range allowedValues {
+		allowed[string(v)] = v
+	}
+
+	for _, t := range in {
+		normalized := strings.ToUpper(strings.TrimSpace(t))
+		v, ok := allowed[normalized]
+		if !ok {
+			return nil, fmt.Errorf("invalid %s %q", label, t)
+		}
+		ret = append(ret, v)
+	}
+
+	return ret, nil
+}
