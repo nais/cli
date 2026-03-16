f0dc285150b087d6f36f1257c20dfb7854c248f0
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-16 10:28:23 +0100

    refactor(tests): remove unnecessary variable declaration in flag tests

--- internal/cliflags/flags_test.go
+++ internal/cliflags/flags_test.go
@@ -88,7 +88,6 @@ func TestUniqueFlagValues(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -141,7 +140,6 @@ func TestCountFlagOccurrences(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
