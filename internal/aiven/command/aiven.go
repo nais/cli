8efcdb036fadf9249b875c5803611c92b2bebe53
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-16 14:29:44 +0100

    fix: remove invalid parent deprecation marker

--- internal/aiven/command/aiven.go
+++ internal/aiven/command/aiven.go
@@ -16,7 +16,6 @@ func Aiven(parentFlags *flags.GlobalFlags) *naistrix.Command {
 	return &naistrix.Command{
 		Name:        "aiven",
 		Title:       "Manage Aiven services.",
-		Deprecated:  naistrix.DeprecatedWithoutReplacement(),
 		StickyFlags: aivenFlags,
 		SubCommands: []*naistrix.Command{
 			create(aivenFlags),
