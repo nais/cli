034eebdbe78b43d8c4c7df1b6538c8657d7bc177
Christer Edvartsen <christer.edvartsen@nav.no>
2026-03-18 17:33:42 +0100

    docs: don't delete the index file

--- mise/tasks/generate/docs.sh
+++ mise/tasks/generate/docs.sh
@@ -2,5 +2,5 @@
 #MISE description="Generate Markdown docs (cli.nais.io)"
 set -euo pipefail
 
-rm -f "$MISE_PROJECT_ROOT"/docs/*.md || true
+rm -f "$MISE_PROJECT_ROOT"/docs/nais_*.md || true
 go run cmd/gen_docs/main.go
