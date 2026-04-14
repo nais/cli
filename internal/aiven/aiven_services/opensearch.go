91feb46b59dba141caeaec47138c3383db6d7a95
Christer Edvartsen <christer.edvartsen@nav.no>
2026-04-14 16:13:16 +0200

    refactor: remove nais aiven create
    All subcommands of nais aiven create has been marked as deprecated, so
    we might as well just remove this.

--- internal/aiven/aiven_services/opensearch.go
+++ internal/aiven/aiven_services/opensearch.go
@@ -2,36 +2,13 @@ package aiven_services
 
 import (
 	"fmt"
-	"strings"
 
 	aiven_nais_io_v1 "github.com/nais/liberator/pkg/apis/aiven.nais.io/v1"
 )
 
 type OpenSearchAccess int64
 
-const (
-	Read OpenSearchAccess = iota
-	Write
-	ReadWrite
-	Admin
-)
-
 var OpenSearchAccesses = []string{"read", "write", "readwrite", "admin"}
-
-func OpenSearchAccessFromString(access string) (OpenSearchAccess, error) {
-	switch strings.ToLower(access) {
-	case OpenSearchAccesses[0]:
-		return Read, nil
-	case OpenSearchAccesses[1]:
-		return Write, nil
-	case OpenSearchAccesses[2]:
-		return ReadWrite, nil
-	case OpenSearchAccesses[3]:
-		return Admin, nil
-	default:
-		return -1, fmt.Errorf("unknown access: %v", access)
-	}
-}
 
 func (p OpenSearchAccess) String() string {
 	return OpenSearchAccesses[p]
