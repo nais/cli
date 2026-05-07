f9b6348b567cc482840abdafd40765775c2e7d35
Christer Edvartsen <christer.edvartsen@nav.no>
2026-05-07 13:46:12 +0200

    refactor: early exit

--- internal/app/command/helpers.go
+++ internal/app/command/helpers.go
@@ -10,22 +10,23 @@ import (
 
 func autoCompleteAppNames(flags *flag.App) naistrix.AutoCompleteFunc {
 	return func(ctx context.Context, args *naistrix.Arguments, _ string) ([]string, string) {
-		if args.Len() == 0 {
-			if len(flags.Team) == 0 {
-				return nil, "Please provide team to auto-complete application names. 'nais defaults set team <team>', or '--team <team>' flag."
-			}
-
-			apps, err := app.GetApplicationNames(ctx, flags.Team)
-			if err != nil {
-				return nil, "Unable to fetch application names."
-			}
-
-			if flags.Environment != "" {
-				return apps.InEnv(string(flags.Environment)), "Select an application."
-			}
-
-			return apps.Unique(), "Select an application. Use --environment to filter by environment."
+		if args.Len() != 0 {
+			return nil, ""
 		}
-		return nil, ""
+
+		if len(flags.Team) == 0 {
+			return nil, "Please provide team to auto-complete application names. 'nais defaults set team <team>', or '--team <team>' flag."
+		}
+
+		apps, err := app.GetApplicationNames(ctx, flags.Team)
+		if err != nil {
+			return nil, "Unable to fetch application names."
+		}
+
+		if flags.Environment != "" {
+			return apps.InEnv(string(flags.Environment)), "Select an application."
+		}
+
+		return apps.Unique(), "Select an application. Use --environment to filter by environment."
 	}
 }
--- internal/job/command/helper.go
+++ internal/job/command/helper.go
@@ -10,22 +10,23 @@ import (
 
 func autoCompleteJobNames(flags *flag.Job) naistrix.AutoCompleteFunc {
 	return func(ctx context.Context, args *naistrix.Arguments, _ string) ([]string, string) {
-		if args.Len() == 0 {
-			if len(flags.Team) == 0 {
-				return nil, "Please provide team to auto-complete job names. 'nais defaults set team <team>', or '--team <team>' flag."
-			}
-
-			if flags.Environment == "" {
-				return nil, "Please provide environment to auto-complete job names. '-e, --environment <environment>' flag."
-			}
-
-			jobs, err := job.GetJobNames(ctx, flags.Team, string(flags.Environment))
-			if err != nil {
-				return nil, "Unable to fetch job names."
-			}
-
-			return jobs, "Select a job."
+		if args.Len() != 0 {
+			return nil, ""
 		}
-		return nil, ""
+
+		if len(flags.Team) == 0 {
+			return nil, "Please provide team to auto-complete job names. 'nais defaults set team <team>', or '--team <team>' flag."
+		}
+
+		if flags.Environment == "" {
+			return nil, "Please provide environment to auto-complete job names. '-e, --environment <environment>' flag."
+		}
+
+		jobs, err := job.GetJobNames(ctx, flags.Team, string(flags.Environment))
+		if err != nil {
+			return nil, "Unable to fetch job names."
+		}
+
+		return jobs, "Select a job."
 	}
 }
