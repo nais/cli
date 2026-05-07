fef1340a854be6b145b6733c4f05538489521e9a
Christer Edvartsen <christer.edvartsen@nav.no>
2026-05-07 11:11:06 +0200

    refactor: remove local environment flag from log

--- internal/log/command/flag/flag.go
+++ internal/log/command/flag/flag.go
@@ -8,8 +8,6 @@ import (
 
 type LogFlags struct {
 	*flags.GlobalFlags
-
-	Environment    string        `name:"environment" short:"e" usage:"Filter logs to a specific |environment|."`
 	WithTimestamps bool          `name:"with-timestamps" usage:"Include timestamps in log output."`
 	WithLabels     bool          `name:"with-labels" usage:"Include labels in log output."`
 	RawQuery       string        `name:"raw-query" usage:"Provide a raw query to filter logs. See https://grafana.com/docs/loki/latest/logql/ for syntax."`
