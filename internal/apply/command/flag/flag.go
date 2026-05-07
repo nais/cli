c3823065ac28f8ebdb83fa9c2103c615f2fa73d0
Christer Edvartsen <christer.edvartsen@nav.no>
2026-05-07 11:22:05 +0200

    refactor: remove local environment flag from apply

--- internal/apply/command/flag/flag.go
+++ internal/apply/command/flag/flag.go
@@ -1,25 +1,9 @@
 package flag
 
 import (
-	"context"
-	"fmt"
-
 	alpha "github.com/nais/cli/internal/alpha/command/flag"
-	"github.com/nais/cli/internal/naisapi"
-	"github.com/nais/naistrix"
 )
-
-type Env string
-
-func (e *Env) AutoComplete(ctx context.Context, _ *naistrix.Arguments, _ string, _ any) ([]string, string) {
-	envs, err := naisapi.GetAllEnvironments(ctx)
-	if err != nil {
-		return nil, fmt.Sprintf("Failed to fetch environments for auto-completion: %v", err)
-	}
-	return envs, "Available environments"
-}
 
 type Apply struct {
 	*alpha.Alpha
-	Environment Env `name:"environment" short:"e" usage:"The |ENVIRONMENT| to apply resources to."`
 }
