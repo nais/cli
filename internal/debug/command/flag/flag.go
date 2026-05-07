b1615caf37388776e7fc274912ac7ed53c182d22
Christer Edvartsen <christer.edvartsen@nav.no>
2026-05-07 10:08:13 +0200

    refactor: remove local environment flag from debug

--- internal/debug/command/flag/flag.go
+++ internal/debug/command/flag/flag.go
@@ -1,31 +1,15 @@
 package flag
 
 import (
-	"fmt"
-
 	"github.com/nais/cli/internal/flags"
 )
 
 type (
-	Environment string
 	DebugSticky struct {
 		*flags.GlobalFlags
-		Copy        bool        `name:"copy" usage:"Create a copy of the pod with a debug container. The original pod remains running and unaffected."`
-		Environment Environment `name:"environment" short:"e" usage:"The environment to use."`
-		Context     string      `name:"context" short:"c" usage:"REMOVED, see --environment."`
-		Namespace   string      `name:"namespace" short:"n" usage:"REMOVED, see --team."`
+		Copy bool `name:"copy" usage:"Create a copy of the pod with a debug container. The original pod remains running and unaffected."`
 	}
 )
-
-func (d DebugSticky) UsesRemovedFlags() error {
-	if d.Namespace != "" {
-		return fmt.Errorf("the -n, --namespace flag is replaced with the -t, --team flag")
-	}
-	if d.Context != "" {
-		return fmt.Errorf("the -c, --context flag is replaced with the -e, --environment flag")
-	}
-	return nil
-}
 
 type Debug struct {
 	*DebugSticky
